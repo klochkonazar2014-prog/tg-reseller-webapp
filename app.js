2b80207b564d1d4c4d601ec8b35ff0a045a29a55
klochkonazar2014-prog <klochkonazar2014@prog.com>
2026-01-15 09:30:17 +0200

    Add logs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const OWNER_WALLET = "UQBxgCx_WJ4_fKgz8tec73NZadhoDzV250-Y0taVPJstZsRl";
 const MANIFEST_URL = "https://klochkonazar2014-prog.github.io/tg-reseller-webapp/tonconnect-manifest.json";
 
 // Tunnel URL
-const BACKEND_URL = "https://ce55b80b1a244ba6-176-119-99-6.serveousercontent.com";
+const BACKEND_URL = "https://81f90ce20d449dea-176-119-99-6.serveousercontent.com";
 
 let tonConnectUI;
 let ALL_MARKET_ITEMS = [];
