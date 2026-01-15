faa6f2461163844f4d4e21195e79ce884de3ecf3
klochkonazar2014-prog <klochkonazar2014@prog.com>
2026-01-15 08:16:41 +0200

    UI: Update tunnel URL and version 1.1.4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const OWNER_WALLET = "UQBxgCx_WJ4_fKgz8tec73NZadhoDzV250-Y0taVPJstZsRl";
 const MANIFEST_URL = "https://klochkonazar2014-prog.github.io/tg-reseller-webapp/tonconnect-manifest.json";
 
 // Tunnel URL
-const BACKEND_URL = "https://403c6cb4a4c05def-176-119-99-6.serveousercontent.com";
+const BACKEND_URL = "https://2580a9469433fe72-176-119-99-6.serveousercontent.com";
 
 let tonConnectUI;
 let ALL_MARKET_ITEMS = [];
