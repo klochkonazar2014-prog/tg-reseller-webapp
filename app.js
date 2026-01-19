37251360fa327247aa6d637882f447390e48cc0b
klochkonazar2014-prog <klochkonazar2014@prog.com>
2026-01-19 13:45:08 +0200

    Auto-update tunnel

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const OWNER_WALLET = "UQBxgCx_WJ4_fKgz8tec73NZadhoDzV250-Y0taVPJstZsRl";
 const MANIFEST_URL = "https://klochkonazar2014-prog.github.io/tg-reseller-webapp/tonconnect-manifest.json";
 
 // Tunnel URL
-const BACKEND_URL = "https://k3sytp-ip-89-117-254-94.tunnelmole.net";
+const BACKEND_URL = "https://4ki7cm-ip-176-119-99-6.tunnelmole.net";
 
 let tonConnectUI;
 let ALL_MARKET_ITEMS = [];
